66349ce7f8043b6da8ccdd6e774135880679b5e2
Thomas Fossati <thomas@linux-0oa2.site>
2015-09-29 16:25:48 +0200

    Added Preferences on login

--- WLTwitterStep0/app/src/main/java/main/WLTwitterActivity.java
+++ WLTwitterStep0/app/src/main/java/main/WLTwitterActivity.java
@@ -1,15 +1,43 @@
 package main;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by thomas on 25/09/15.
  */
-public class WLTwitterActivity extends Activity {
+public class WLTwitterActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String login = this.getIntent().getExtras().getString("login");
+        this.getActionBar().setSubtitle(login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.wltwitter, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.actionLogout){
+            Context context = getApplicationContext();
+            SharedPreferences sharedPreferences = context.getSharedPreferences(String.valueOf(R.string.login_data), Context.MODE_PRIVATE);
+
+            sharedPreferences.edit().remove("login");
+            sharedPreferences.edit().remove("password");
+            sharedPreferences.edit().apply();
+            finish();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- WLTwitterStep0/app/src/main/java/main/WLTwitterLoginActivity.java
+++ WLTwitterStep0/app/src/main/java/main/WLTwitterLoginActivity.java
@@ -1,7 +1,9 @@
 package main;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -16,22 +18,36 @@ public class WLTwitterLoginActivity extends Activity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Context context = getApplicationContext();
+        SharedPreferences sharedPreferences = context.getSharedPreferences(String.valueOf(R.string.login_data), Context.MODE_PRIVATE);
+        String login = sharedPreferences.getString("login", "");
+        String pwd = sharedPreferences.getString("pwd", "");
         findViewById(R.id.loginButton).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (TextUtils.isEmpty(((EditText) findViewById(R.id.loginEditText)).getText()) || TextUtils.isEmpty(((EditText) findViewById(R.id.passwordTextEdit)).getText())) {
+        if (TextUtils.isEmpty(((EditText) findViewById(R.id.loginEditText)).getText())) {
 
-            Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.error_no_login, Toast.LENGTH_LONG).show();
+        } else if (TextUtils.isEmpty(((EditText) findViewById(R.id.passwordTextEdit)).getText())) {
+            Toast.makeText(this, R.string.error_no_password, Toast.LENGTH_LONG).show();
         } else {
-            String login = ((EditText) findViewById(R.id.loginEditText)).getText().toString();
-            String pwd = ((EditText) findViewById(R.id.passwordTextEdit)).getText().toString();
-            Intent intent = new Intent(this, WLTwitterLoginActivity.class);
-            startActivity(intent);
+            String login = String.valueOf(((EditText) findViewById(R.id.loginEditText)).getText());
+            String pwd = String.valueOf(((EditText) findViewById(R.id.passwordTextEdit)).getText());
+
+            Context context = getApplicationContext();
+            SharedPreferences sharedPreferences = context.getSharedPreferences(String.valueOf(R.string.login_data), Context.MODE_PRIVATE);
+            sharedPreferences.edit().putString("login", login);
+            sharedPreferences.edit().putString("pwd", pwd);
+            sharedPreferences.edit().apply();
+            Intent intent = new Intent(this, WLTwitterActivity.class);
+
             Bundle extras = new Bundle();
             extras.putString("login", login);
             extras.putString("pwd", pwd);
+            intent.putExtras(extras);
+            startActivity(intent);
         }
 
     }
